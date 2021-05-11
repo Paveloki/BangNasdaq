8b85f02bffdcfcd3203ddf130bb64b88ed909403
Paveloki <35500944+Paveloki@users.noreply.github.com>
2021-05-11 14:10:44 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,20 @@
-
 import pandas as pd
 import yfinance as yf
+import datetime
 
+now = datetime.datetime.now()
 tickers = pd.read_csv('CNDX_holdings.csv', skiprows=2).dropna()
 tickers = tickers['Ticker']
 
 precios = {}
 for i in tickers:
-    pr = yf.download(i, start='2015-01-01', end='2021-02-08', interval='1d')
+    pr = yf.download(i, start='2018-01-01', end=now, interval='1d')
+    pr = pr.drop(['Adj Close'], axis=1)
     pr = pr.reset_index()
     precios[i] = pr
+
+NASDAQ = yf.download('^IXIC', start='2018-01-01', end=now, interval='1d')
+NASDAQ = NASDAQ.drop(['Adj Close'], axis=1)
+NASDAQ = NASDAQ.reset_index()
+
+rf = 0.015
